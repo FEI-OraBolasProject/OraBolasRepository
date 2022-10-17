e1136d17c5e6afbd36a8f48ce26b3a3ea4c410ab
turolaruan <turolaruan@gmail.com>
2022-10-17 18:02:48 -0300

    grafico aceleracao
    Co-Authored-By: Ana Beatriz Tavares Malheiro <84588132+AnaBeatrizTavaresMalheiro@users.noreply.github.com>
    Co-Authored-By: BRUNO ANDWELE ALVES ANTUNES <bruno.3a@outlook.com>

---
+++ Dados_Grafico/Gerar_Grafico/cx_cy_aceleracao_robo.py
@@ -0,0 +1,38 @@
+from cProfile import label
+from re import X
+import sys
+import matplotlib
+import tkinter
+matplotlib.use('TkAgg')
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+#separar em listas as linhas do excel
+dataset = open("cx_cy_aceleracao_robo.txt", "r")
+linhas = dataset.readlines()
+
+ListaLinhas = []
+tempo = []
+cx = []
+cy = []
+
+for i in range(1,len(linhas)):
+    ListaLinhas.append(linhas[i].strip("\n").replace(",",".").split("\t"))
+
+for item in ListaLinhas:
+    tempo.append(float(item[0]))
+    cx.append(float(item[1]))
+    cy.append(float(item[2]))
+
+print(tempo)
+print(cx)
+print(cy)
+
+plt.plot(tempo,cx)
+plt.plot(tempo,cy)
+
+plt.title("Aceleracao X e Y do Robo em funcao do Tempo")
+plt.xlabel("Tempo (s)")
+plt.ylabel("Distancia (m)")
+plt.show()
